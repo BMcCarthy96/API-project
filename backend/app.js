854b32a5b9d8ecea06cf83fe85a9fd1640e447ea
Brandon McCarthy <Bmac96.dev@gmail.com>
2024-10-25 21:24:45 -0400

    Merging history

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,16 @@
 const express = require("express");
+const routes = require("./routes");
 require("express-async-errors");
 const morgan = require("morgan");
 const cors = require("cors");
 const csurf = require("csurf");
 const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
-
 const { environment } = require("./config");
 const isProduction = environment === "production";
+const { ValidationError } = require("sequelize");
 
 const app = express();
-const routes = require("./routes");
 
 // Logging middleware
 app.use(morgan("dev"));
@@ -45,6 +45,62 @@ app.use(
     })
 );
 
-app.use(routes); // Connect all the routes
+// Connect all the routes
+app.use(routes);
+
+// Importing routes
+const favoritesRoute = require("./routes/api/favorites");
+const countriesRoutes = require("./routes/api/countries");
+const amenitiesRoutes = require("./routes/api/amenities");
+const bookingsRoute = require("./routes/api/bookings");
+const reviewsRoute = require("./routes/api/reviews");
+const spotImagesRoute = require("./routes/api/spotImages");
+const spotsRoute = require("./routes/api/spots");
+const usersRoute = require("./routes/api/users");
+
+//Using routes
+app.use("/favorites", favoritesRoute);
+app.use("/countries", countriesRoutes);
+app.use("/amenities", amenitiesRoutes);
+app.use("/bookings", bookingsRoute);
+app.use("/reviews", reviewsRoute);
+app.use("/spotImages", spotImagesRoute);
+app.use("/spots", spotsRoute);
+app.use("/users", usersRoute);
+
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = { message: "The requested resource couldn't be found." };
+    err.status = 404;
+    next(err);
+});
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+        let errors = {};
+        for (let error of err.errors) {
+            errors[error.path] = error.message;
+        }
+        err.title = "Validation error";
+        err.errors = errors;
+    }
+    next(err);
+});
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || "Server Error",
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack,
+    });
+});
 
 module.exports = app;
--- backend/routes/spotImages.js
+++ backend/routes/spotImages.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { Spot, SpotImage } = require("../db/models");
+const { Spot, SpotImage } = require("../../db/models");
 
 // Add an image to a spot
 router.post("/:spotId/images", async (req, res) => {
-    const { imageUrl } = req.body;
     try {
+        const { imageUrl } = req.body;
         const spot = await Spot.findByPk(req.params.spotId);
         if (spot) {
             const newImage = await SpotImage.create({
